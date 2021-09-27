e7cdb5f62e173c5628718e02820b5afe8d913a77
Genese Dev <genesedev@MacBook-Pro-de-Genese.local>
2021-09-27 00:50:21 -0300

    Update project for version 12

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,10 @@
 export const environment = {
   production: true,
-  API: 'https://swapi.co/api/',
-  BASE_URL: '/inv',
+    API: 'https://swapi.dev/api/',
+
+    BASE_URL: '/api',
+    // Credenciais para Google Search Images
+    GOOGLE_API_KEY: 'AIzaSyBqRamhsHRXYVY8-71RltemHL-yhr2lNio',
+    GOOGLE_API_URL: 'https://www.googleapis.com/customsearch/v1?key=',
+    GOOGLE_ID: '017576662512468239146:omuauf_lfve'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,10 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
   production: false,
-  GCS_API_KEY: 'AIzaSyBqRamhsHRXYVY8-71RltemHL-yhr2lNio',
-  GCS_API_URL: 'https://www.googleapis.com/customsearch/v1?key=',
-  GCS_ID: '017576662512468239146:omuauf_lfve'
+    API: 'https://swapi.dev/api/',
+
+    BASE_URL: '/api',
+    // Credenciais para Google Search Images
+    GOOGLE_API_KEY: 'AIzaSyBqRamhsHRXYVY8-71RltemHL-yhr2lNio',
+    GOOGLE_API_URL: 'https://www.googleapis.com/customsearch/v1?key=',
+    GOOGLE_ID: '004869299073777913558:vp5ui4xprny'    
 };
