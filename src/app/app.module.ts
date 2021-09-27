8b48718af9f20c1e31c2b415d239ed427f1b3bdf
Genese Dev <genesedev@MacBook-Pro-de-Genese.local>
2021-09-27 00:13:59 -0300

    Update projet for Angular version 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ImagesListComponent } from './images-list/images-list.component';
+import { ImagesServicesComponent, ImagesService } from './images-services/images-services.component';
 import { FooterComponent } from './footer/footer.component';
-import { PersonComponent } from './person/person.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
-import { MatDialog } from '@angular/material';
-import { Overlay, OverlayModule } from '@angular/cdk/overlay';
-import {MatDialogModule} from '@angular/material/dialog';
-
+import {enableProdMode} from '@angular/core';
+enableProdMode();
 @NgModule({
-    declarations: [
-        AppComponent,
-        ListComponent,
-        PersonComponent,
-        HeaderComponent,
-        FooterComponent,
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        SharedModule,
-        MatDialogModule
-    ],
-    entryComponents: [
-        PersonComponent
-    ],
-    providers: [ 
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ImagesListComponent,
+    ImagesServicesComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    ImagesService,
+    HttpClientModule],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
