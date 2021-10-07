aa3ce1b09791d9a2e730669be94661d83ac8b497
Genese Dev <genesedev@MacBook-Pro-de-Genese.local>
2021-10-07 17:01:13 -0300

    clone and update in local machine

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
 
     BASE_URL: '/api',
     // Credenciais para Google Search Images
-    GOOGLE_API_KEY: 'Here your key',
-    GOOGLE_API_URL: 'Here your key',
-    GOOGLE_ID: 'Here your key',
+    GOOGLE_API_KEY: 'AIzaSyBqRamhsHRXYVY8-71RltemHL-yhr2lNio',
+    GOOGLE_API_URL: 'https://www.googleapis.com/customsearch/v1?key=',
+    GOOGLE_ID: '004869299073777913558:vp5ui4xprny'    
 };
