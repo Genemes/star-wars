e9b5dcdbe3ec5638e3c655d36574b51e96268059
Genese Dev <genesedev@MacBook-Pro-de-Genese.local>
2021-10-09 16:07:29 -0300

    merge master

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   BASE_URL: '/api',
   // Credenciais para Google Search Images
   GOOGLE_API_KEY: 'YOUR_KEY',
-  GOOGLE_API_URL: 'YORK_KEY',
+  GOOGLE_API_URL: 'YOUR_KEY',
   GOOGLE_ID: 'YOUR_KEY'    
 };
